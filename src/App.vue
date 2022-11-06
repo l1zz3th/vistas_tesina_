1c713a3a651c40ea24b3a6d88073b8c1192ed8f3
l1zz3th <49285964+l1zz3th@users.noreply.github.com>
2022-11-06 02:47:13 -0600

    s

--- src/App.vue
+++ src/App.vue
@@ -4,60 +4,68 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  
+<div class="contenedor">
   <h1 class="title">Inicia Sesion</h1>
-<header>
+
+<Content>
+    
     <img alt="Fondo" class="img-fondo" src="@/assets/images/fondo.jpg"  />
 
-    <div class="form">
-      
-    <form action class="form" @submit.prevent="register">
-      <label class="form-label" for="#email">Email:</label>
-      <input
-        v-model="email"
-        class="form-input"
-        type="email"
-        id="email"
-        required
-        placeholder="Email"
-      >
-      <label class="form-label" for="#password">Password:</label>
-      <input
-        v-model="password"
-        class="form-input"
-        type="password"
-        id="password"
-        placeholder="Password"
-      >
-      <label class="form-label" for="#password-repeat">Repite la contraeña:</label>
-      <input
-        v-model="passwordRepeat"
-        class="form-input"
-        type="password"
-        id="password-repeat"
-        placeholder="Password"
-      >
-      <input class="form-submit" type="submit" value="Sign Up">
-    </form>
+<div>
+      <form action class="form" >
+        <label class="form-label" for="#email">Email:</label>
+        <input
+          v-model="email"
+          class="form-input"
+          type="email"
+          id="email"
+          required
+          placeholder="Email"
+        >
+        <label class="form-label" for="#password">Password:</label>
+        <input
+          v-model="password"
+          class="form-input"
+          type="password"
+          id="password"
+          placeholder="Password"
+        >
+
+
+        
+        <input class="form-submit" type="submit" value="Iniciar Sesion">
+      </form>
     </div>
-</header>
+</Content>
+</div>
+
 
 </template>
 
 <style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.contenedor{
+  background: white;
+  width: 3rem auto;
+  height: 3rem auto;
+  align-content: center;
+  display: block;
+
+}
+content {
+  display: flex;
+    align-items: center;
+    height: 100vh;
+    margin: 0 auto;
+    max-width: 1200px;
+    width: 95%;  
 }
 
 h1 {
   text-align: center;
-  text-color: black;
+  color: black;
 	width: auto;
-	height: auto;
+	height: 70px;
 	padding: 8px 8px 8px 8px;
 	background: #ffebd2;
 	border-color: #ffebd2;
@@ -65,28 +73,69 @@ h1 {
 	border-style: solid;
 }
 
-
-header{
-  background-color: white;
-  border: black;
-  width:auto;
-  height:auto;
-}
 .img-fondo {
-  color: #fff;
-  align-content: left;
-}
-
-.form {
-  text-align: left;
-  color: #da5526;
+    background-position: left left;
+    background-size: cover;
+    height: auto;
+    width: auto;
+    flex: 0 0 calc(60%);
+    position: relative
 }
 
 .tittle {
   text-align: center;
 
+}.form {
+  margin: 3rem auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: auto;
+  min-width: auto;
+  max-width: auto;
+  background: rgba(250, 253, 255, 0.9);
+  border-radius: 5px;
+  padding: 40px;
+  box-shadow: 0 4px 10px 4px rgba(0, 0, 0, 0.3);
+}
+.form-label {
+	width: 154px;
+	height: 60px;
+	color: #da5526;
+	font-family: "Arial";
+	font-size: large;
+	text-align: left;
+}
+
+.form-input {
+  padding: 10px 15px;
+  border: 1px solid rgb(0, 0, 0);
+  color: rgb(2, 2, 2);
+  
+}
+.form-submit {
+  background: #da810d;
+  border: none;
+  color: white;
+  margin-top: 3rem;
+  padding: 1rem 0;
+  cursor: pointer;
+  
+}
+.error {
+  margin: 1rem 0 0;
+  color: #ff4a96;
+}
+.button {
+	width: 415px;
+	height: 107px;
+	padding: 8px 8px 8px 8px;
+	box-shadow: 0px 5px 10px rgba(248, 95, 106, 0.23);
+	background: #f68930;
+	border-radius: 6px 6px 6px 6px;
 }
 
 
 
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa&family=Libre+Franklin:wght@300&display=swap" rel="stylesheet');
 </style>
